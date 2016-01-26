540c594183ede4c6a41b4b0494ad30ab07dbb281
Kuba Stawiarski <kuba.stawiarski@gmail.com>
2016-01-26 23:01:37 +0000

    Add slug enforcement
    I implemented a custom redirect method, but ideally the redirection should be handled on a much higher level of abstraction.
    Unfortunately no unit tests have been added, as these are not set up in the application.

--- src/FizzBuzz/Controller/AbstractController.php
+++ src/FizzBuzz/Controller/AbstractController.php
@@ -4,6 +4,7 @@ namespace FizzBuzz\Controller;
 
 use FizzBuzz\Renderer;
 use Smrtr\SpawnPoint\AbstractController as SpawnAbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 abstract class AbstractController extends SpawnAbstractController
 {
@@ -15,5 +16,35 @@ abstract class AbstractController extends SpawnAbstractController
     public function __construct()
     {
         $this->tpl = new Renderer;
+    }
+
+    /**
+     * Redirects to a specific route
+     *
+     * @param string $route
+     * @param array $params
+     * @param int $status
+     * @throws \Smrtr\HaltoRouterException
+     */
+    protected function redirectToRoute($route, $params = [], $status = Response::HTTP_FOUND)
+    {
+        $url = $this->app->router->generate($route, $params);
+        $this->redirect($url, $status);
+    }
+
+    /**
+     * Redirects to given URL
+     *
+     * @param string $url
+     * @param int $status
+     */
+    protected function redirect($url, $status = Response::HTTP_FOUND)
+    {
+        $location = $this->getRequest()->server->get('REQUEST_SCHEME') . ':' . $url;
+
+        // ideally, we should just return an instance of \Symfony\Component\HttpFoundation\RedirectResponse
+        // and \Smrtr\SpawnPoint\App should handle that and perform the redirection
+        header('Location: ' . $location, true, $status);
+        exit;
     }
 }
--- src/FizzBuzz/Controller/Article.php
+++ src/FizzBuzz/Controller/Article.php
@@ -7,6 +7,8 @@ class Article extends AbstractController
     public function view()
     {
         $articleID = (int) $this->getRoutedParam('id');
+        $slug = $this->getRoutedParam('slug');
+
         if (!$articleID) {
             throw new \Exception("Article ID is invalid", 404);
         }
@@ -16,6 +18,10 @@ class Article extends AbstractController
 
         if (!is_array($article) or !count($article)) {
             throw new \Exception("Article not found", 404);
+        }
+
+        if ($article['slug'] !== $slug) {
+            $this->redirectToRoute('article-view', ['id' => $articleID, 'slug' => $article['slug']]);
         }
 
         $this->tpl->article = $article;
