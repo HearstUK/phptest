0cb59158f0497f74b573d5e94cd35b6a55409fde
Oliver Johnstone <oliver.johnstone@clock.co.uk>
2016-02-25 21:41:18 +0000

    Enforcing correct slug and redirecting when article is found with correct id but incorrect slug

--- src/FizzBuzz/Controller/Article.php
+++ src/FizzBuzz/Controller/Article.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace FizzBuzz\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse as RedirectResponse;
 
 class Article extends AbstractController
 {
@@ -16,6 +17,14 @@ class Article extends AbstractController
 
         if (!is_array($article) or !count($article)) {
             throw new \Exception("Article not found", 404);
+        }
+
+        if ($article['slug'] !== $this->getRoutedParam('slug')) {
+            $redirectParams = array(
+                'id' => $article['id'],
+                'slug' => $article['slug']
+            );
+            return (new RedirectResponse($this->app->router->generate('article-view', $redirectParams)))->send();
         }
 
         $this->tpl->article = $article;
